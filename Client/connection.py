aaa1d163c4586df9ca9e05be89423cff80692f53
mkwarta <Michu1164@gmail.com>
2022-01-23 15:10:01 +0100

    formatted with black

--- Client/connection.py
+++ Client/connection.py
@@ -1,11 +1,9 @@
+import logging
 import select
 import socket
 import threading
 from os.path import exists
 from queue import Queue
-import logging
-
-
 
 
 class Communication:
@@ -13,7 +11,13 @@ class Communication:
     timeLimit: int = 15
     connectionStable: bool = True
 
-    def __init__(self, GUIReference=None, address: str = '127.0.0.1', port: int = 2137, isHost: bool = False):
+    def __init__(
+        self,
+        GUIReference=None,
+        address: str = "127.0.0.1",
+        port: int = 2137,
+        isHost: bool = False,
+    ):
         self.address: str = address
         self.port: int = port
         self.isHost: bool = isHost
@@ -21,83 +25,70 @@ class Communication:
 
     def listen(self, s: socket):
         while self.connectionStable:
-            
+
             ready_to_read_message, _, _ = select.select([s], [], [], self.timeLimit)
-            
-            
+
             if ready_to_read_message:
-                
-                msg_size_bytes  = self.read_n_bytes(s,2)
+
+                msg_size_bytes = self.read_n_bytes(s, 2)
                 if msg_size_bytes:
                     msg_size = int(msg_size_bytes.decode("UTF-8"))
-                    
-                    print(f"msg size {msg_size}",end=' ')
-                    
+
+                    print(f"msg size {msg_size}", end=" ")
+
                 if self.connectionStable:
-                    message = self.read_n_bytes(s,msg_size).decode("UTF-8")
+                    message = self.read_n_bytes(s, msg_size).decode("UTF-8")
                     print(f"message {message}")
                     self.handleMessage(Message(message))
-            
-                    
-
-                
-
 
             else:
                 print("timed out")
                 # TODO: handling timeout + change time limit to 30(?)
 
-
-
-    def read_n_bytes(self,s:socket,bytes_count:int) ->bytes:
+    def read_n_bytes(self, s: socket, bytes_count: int) -> bytes:
         chunks = []
         bytes_recd = 0
-        while bytes_recd<bytes_count:
+        while bytes_recd < bytes_count:
             try:
-                chunk = s.recv(bytes_count-bytes_recd)
+                chunk = s.recv(bytes_count - bytes_recd)
                 chunks.append(chunk)
-                bytes_recd+=len(chunk)
-                print(chunks,chunk)
-                if chunk ==b'':
-                    self.connectionStable = False 
-                    self.GUI.setErrorScene("Server closed connection",allowReconnect=True)
+                bytes_recd += len(chunk)
+                print(chunks, chunk)
+                if chunk == b"":
+                    self.connectionStable = False
+                    self.GUI.setErrorScene(
+                        "Server closed connection", allowReconnect=True
+                    )
                     return None
-
             except socket.timeout as e:
-                err = e.args[0]
                 logging.error(e)
-                
-                if err == 'timed out':
-                    #TODO: HANDLING TIMEOUT INSIDE MESSAGE
-                    #moze liczenie timeoutów ma sens? Jeśli wewnatrz wiadomosc timeout x3 to connection stable false?
+                if e == "timed out":
+                    # TODO: HANDLING TIMEOUT INSIDE MESSAGE
+                    # moze liczenie timeoutów ma sens? Jeśli wewnatrz wiadomosc
+                    # timeout x3 to connection stable false?
                     pass
-                
-                    #self.connectionStable = False #timeout
-                    
             except socket.error as e:
-                self.connectionStable = False #jakis bardzo zły błąd
+                self.connectionStable = False  # jakis bardzo zły błąd
                 logging.error(e)
                 break
-            
-            
-            
-        return b''.join(chunks)
-            
-    def mysendall(self,s:socket,data:str):
+
+        return b"".join(chunks)
+
+    def mysendall(self, s: socket, data: str):
         ret = 0
-        while ret<len(data):
+        while ret < len(data):
             try:
                 sent = s.send(data[ret:])
-                ret+=sent
+                ret += sent
             except socket.timeout as e:
                 logging.error(e)
-                #FIXME: HANDLE TIMEOUT
+                # FIXME: HANDLE TIMEOUT
             except socket.error as e:
                 logging.error(e)
                 self.conectionstable = False
                 break
         return ret
-    
+
     def write(self, s: socket):
 
         # it is blocking, but it blocks in a thread, so it doesnt really matter
@@ -107,9 +98,9 @@ class Communication:
         while self.connectionStable:
             message: str = self.messageQueue.get(block=True)
             _, ready_to_write, _ = select.select([], [s], [], self.timeLimit)
-            
+
             if ready_to_write:
-                self.mysendall(s,(str(message)).encode("UTF-8"))
+                self.mysendall(s, (str(message)).encode("UTF-8"))
             else:
                 print("timed out")
                 # TODO: handling timeout + change time limit to 30(?)
@@ -120,7 +111,7 @@ class Communication:
     def getMessage(self) -> "Message":
         try:
             msg = self.readQueue.get(block=True, timeout=self.timeLimit)
-        except:
+        except BaseException:
             msg = "9##!"
         return msg
 
@@ -128,12 +119,12 @@ class Communication:
         self.messageQueue.queue.clear()
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(5)
-            
+
             try:
                 s.connect((self.address, self.port))
-            except (socket.timeout,ConnectionRefusedError) as e:
+            except (socket.timeout, ConnectionRefusedError) as e:
                 logging.error(e)
-                self.GUI.setErrorScene("Couldn't connect to the server",True)
+                self.GUI.setErrorScene("Couldn't connect to the server", True)
                 return
             readerThread = threading.Thread(target=self.listen, args=(s,))
             writerThread = threading.Thread(target=self.write, args=(s,))
@@ -145,66 +136,66 @@ class Communication:
             writerThread.join()
 
     def handleMessage(self, msg: "Message") -> None:
-        print(msg.code,msg.text)
-        if msg.code ==msg.new_player:
+        print(msg.code, msg.text)
+        if msg.code == msg.new_player:
             if msg.text.isnumeric():  # received id, need to save it to file
                 with open("id.txt", "w+") as f:
                     f.write(msg.text)
             else:
                 self.GUI.setErrorScene(
-                    '''This nick is taken!
-                    \nPlease connect once more with different name ;-)''')
-                
-        elif msg.code ==msg.ready_code:
+                    """This nick is taken!
+                    \nPlease connect once more with different name ;-)"""
+                )
+
+        elif msg.code == msg.ready_code:
             self.GUI.QtStack.setCurrentWidget(self.GUI.GameScene)
-            
-            
+
         elif msg.code == msg.new_host:
-            
+
             if msg.text.isnumeric():  # received id, need to save it to file
                 self.GUI.QtStack.setCurrentWidget(self.GUI.GameScene)
-                
-                #FIXME: Czy host musi zapisywać id? Gra bez hosta chyba powinna sie skończyć
+
+                # FIXME: Czy host musi zapisywać id? Gra bez hosta chyba powinna sie skończyć
                 # with open("id.txt", "w+") as f:
                 #     f.write(msg.text)
             else:
                 self.GUI.setErrorScene(
-                    '''This nick is taken!
-                    \nPlease connect once more with different name ;-)''')
-        
-            
+                    """This nick is taken!
+                    \nPlease connect once more with different name ;-)"""
+                )
 
-
-        elif msg.code ==msg.guessed_letter:
-            player, guessed,missed = msg.text.split(':')
+        elif msg.code == msg.guessed_letter:
+            player, guessed, missed = msg.text.split(":")
             self.GUI.playersDict[player] = f"{guessed}/{missed}"
             self.GUI.updateLeaderBoard()
 
-        elif msg.code ==msg.winner_code:
-            #TODO: 
+        elif msg.code == msg.winner_code:
+            # TODO:
             pass
 
-        elif msg.code ==msg.reconnect_code:
-            
-                if msg.text == "sendID":
-                    if exists("id.txt"):
-                        with open("id.txt", "r+") as f:
-                            id = f.readline()
-                        # send id to let server verify if i was connected
-                        self.addTexttoQueue(Message(id, msg.reconnect_code))
-                    else:  # cant find id file
-                        self.GUI.setErrorScene(
-                            "You weren't playing in this game\n Please wait for the game to end")
+        elif msg.code == msg.reconnect_code:
 
-                elif msg.text == "valid":
-                    self.GUI.QtStack.setCurrentWidget(self.GUI.GameScene)
-
-                elif msg.text == "invalid":
+            if msg.text == "sendID":
+                if exists("id.txt"):
+                    with open("id.txt", "r+") as f:
+                        id = f.readline()
+                    # send id to let server verify if i was connected
+                    self.addTexttoQueue(Message(id, msg.reconnect_code))
+                else:  # cant find id file
                     self.GUI.setErrorScene(
-                        '''Someone else was playing under this nickname,
-                        \nEnter your nickname or wait for the game to end''')
-                else:
-                    print("No idea what happened")
+                        "You weren't playing in this game\n Please wait for the game to end"
+                    )
+
+            elif msg.text == "valid":
+                self.GUI.QtStack.setCurrentWidget(self.GUI.GameScene)
+
+            elif msg.text == "invalid":
+                self.GUI.setErrorScene(
+                    """Someone else was playing under this nickname,
+                        \nEnter your nickname or wait for the game to end"""
+                )
+            else:
+                print("No idea what happened")
 
         elif msg.code == msg.new_password:
             self.GUI.setPassword(msg.text)
@@ -216,7 +207,7 @@ class Communication:
 
 
 class Message:
-    #TODO: ZMIENIC NA ZNAKI
+    # TODO: ZMIENIC NA ZNAKI
     new_host = 1
     new_player = 2
     ready_code = 3
@@ -224,8 +215,6 @@ class Message:
     winner_code = 5
     reconnect_code = 6
     new_password = 7
-    
-    
 
     def __init__(self, text: str, code=None):
         if code:
@@ -235,8 +224,7 @@ class Message:
         else:
             self.code = int(text[0])
             self.text = text[1:]
-        self.length = str(len(self.text)+1)  # +1 because of code 
+        self.length = str(len(self.text) + 1)  # +1 because of code
 
     def __str__(self):
         return f"{self.length.zfill(2)}{self.code}{self.text}"
-
--- Client/main.py
+++ Client/main.py
@@ -11,16 +11,12 @@ from connection import Communication, Message
 from FlowLayout import FlowLayout
 
 
-
-
-
-
 class MainWindow(qtw.QDialog):
-    life = ['./resources/'+f'life{i}.png' for i in range(5)]
+    life = ["./resources/" + f"life{i}.png" for i in range(5)]
     alphabet = list("abcdefghijklmnopqrstuwxyz".upper())
     lifeCounter = -len(alphabet)  # buttons fire callbacks when created
     password = "_".upper()
-    guessedpassword = ['_' for _ in password]
+    guessedpassword = ["_" for _ in password]
     passwordLabel = 0
     connected = False
     playersCount = 0
@@ -31,7 +27,7 @@ class MainWindow(qtw.QDialog):
         self.setWindowIcon(QIcon("./resources/LogoBlue.png"))
         self.setupUi()
         with open("config.txt", "r+") as f:
-                self.ip = f.readline()
+            self.ip = f.readline()
 
     def setupUi(self):
 
@@ -43,9 +39,8 @@ class MainWindow(qtw.QDialog):
         self.GameScene = qtw.QWidget()
         self.ErorrScene = qtw.QWidget()
 
-        stylesheet = open('stylesheet.css').read()
-        scenes = [self.WelcomeScene, self.WaitingRoom,
-                  self.GameScene, self.ErorrScene]
+        stylesheet = open("stylesheet.css").read()
+        scenes = [self.WelcomeScene, self.WaitingRoom, self.GameScene, self.ErorrScene]
         for scene in scenes:
             scene.setStyleSheet(stylesheet)
             self.QtStack.addWidget(scene)
@@ -71,30 +66,37 @@ class MainWindow(qtw.QDialog):
         hostButton = qtw.QPushButton("I'm a host")
         connectButton = qtw.QPushButton("I want to join")
 
-        
         nameField = qtw.QLineEdit()
         nameField.setMaxLength(20)  # set max name legnth to 20 chars
         nameField.setPlaceholderText("Enter nickname:")
         nameField.setMinimumWidth(100)
         nameField.setAlignment(Qt.Qt.AlignCenter)
 
-        hostButton.clicked.connect(lambda:
-                                   self.goToWaitingRoom("Waiting for players...", nameField, hostButton=True))
+        hostButton.clicked.connect(
+            lambda: self.goToWaitingRoom(
+                "Waiting for players...", nameField, hostButton=True
+            )
+        )
 
-        connectButton.clicked.connect(lambda :
-           self.goToWaitingRoom("Waiting for players...",nameField,hostButton=False)) 
-        
         connectButton.clicked.connect(
-            lambda: self.QtStack.setCurrentWidget(self.GameScene))
+            lambda: self.goToWaitingRoom(
+                "Waiting for players...", nameField, hostButton=False
+            )
+        )
+
+        connectButton.clicked.connect(
+            lambda: self.QtStack.setCurrentWidget(self.GameScene)
+        )
 
         layout.addWidget(hostButton, 1, 0)
         layout.addWidget(connectButton, 1, 1)
-        layout.addWidget(nameField, 0, 0,1,2)
+        layout.addWidget(nameField, 0, 0, 1, 2)
 
         for id in range(layout.count()):
             item = layout.itemAt(id).widget()
-            item.setSizePolicy(qtw.QSizePolicy(qtw.QSizePolicy.Ignored,
-                                               qtw.QSizePolicy.Ignored))
+            item.setSizePolicy(
+                qtw.QSizePolicy(qtw.QSizePolicy.Ignored, qtw.QSizePolicy.Ignored)
+            )
             item.setMaximumHeight(100)
 
         self.WelcomeScene.setLayout(layout)
@@ -103,14 +105,13 @@ class MainWindow(qtw.QDialog):
         btn.setDown(True)
         btn.setStyleSheet('color: "#E5E5E5"')
         # FIXME: READY MESSAGE TO SERVER
-        self.com.addTexttoQueue(Message('1', Message.ready_code))
+        self.com.addTexttoQueue(Message("1", Message.ready_code))
 
     def updatePlayersInfo(self):
         if self.playersCount == 1:
             self.playersCountInfo.setText("Currently 1 player")
         else:
-            self.playersCountInfo.setText(
-                f"Currently {self.playersCount} players")
+            self.playersCountInfo.setText(f"Currently {self.playersCount} players")
 
     def initWaitingRoom(self):
         self.ready = 0
@@ -137,7 +138,8 @@ class MainWindow(qtw.QDialog):
     def initErrorScene(self):
         layout = qtw.QGridLayout()
         textLabel = qtw.QLabel(
-            "There was problem with server.\n You can try to reconnect or go back to main menu")
+            "There was problem with server.\n You can try to reconnect or go back to main menu"
+        )
         textLabel.setAlignment(Qt.Qt.AlignCenter)
         readyButton = qtw.QPushButton("Try to Reconnect")
         cancelButton = qtw.QPushButton("Go Back to main menu")
@@ -154,35 +156,28 @@ class MainWindow(qtw.QDialog):
         self.com = None
 
     def goToWaitingRoom(self, text: str, name: str, hostButton=False):
-        
 
-        
-
-        if not(name.text().isalnum()):
+        if not (name.text().isalnum()):
             name.setText("Please enter valid nickname")
             return
 
         # initialize comunication
-        
+
         self.QtStack.setCurrentWidget(self.WaitingRoom)
 
         self.com = Communication(
-            GUIReference=self, address=self.ip, port=2137, isHost=hostButton)
-        
+            GUIReference=self, address=self.ip, port=2137, isHost=hostButton
+        )
+
         threading.Thread(target=self.com.run).start()
-        
-    
-        self.WaitingRoom.findChild(qtw.QLabel).setText(
-        "Waiting for other players...")
+
+        self.WaitingRoom.findChild(qtw.QLabel).setText("Waiting for other players...")
         if hostButton:
             msg = Message(name.text(), Message.new_host)
         else:
             msg = Message(name.text(), Message.new_player)
-        
+
         self.com.addTexttoQueue(msg)
-            
-        
-        
 
     def updateLeaderBoard(self):
         items = 0
@@ -202,8 +197,7 @@ class MainWindow(qtw.QDialog):
         self.HangmanImage = qtw.QLabel()
         layout = qtw.QGridLayout()
         playersTable = qtw.QTableWidget()  # Table widget IS scrollable by default
-        playersDict = {"Player1": 1, "Player2": 5,
-                       "p3": 123, "p5": 2137, "p4": 12213}
+        playersDict = {"Player1": 1, "Player2": 5, "p3": 123, "p5": 2137, "p4": 12213}
         self.playersDict = playersDict
         self.playersTable = playersTable
 
@@ -211,13 +205,14 @@ class MainWindow(qtw.QDialog):
         playersTable.setHorizontalHeaderLabels(["Name", "score"])
         playersTable.verticalHeader().hide()
         playersTable.setHorizontalScrollBarPolicy(
-            Qt.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+            Qt.Qt.ScrollBarPolicy.ScrollBarAlwaysOff
+        )
 
         self.updateLeaderBoard()
 
         layout.addWidget(playersTable, 0, 0, 5, 2)
 
-        self.passwordLabel = qtw.QLabel(' '.join(self.guessedpassword))
+        self.passwordLabel = qtw.QLabel(" ".join(self.guessedpassword))
         gameMainWidget = qtw.QWidget()
         hangmanLayout = qtw.QVBoxLayout()
         self.HangmanImage.setAlignment(Qt.Qt.AlignCenter)
@@ -235,24 +230,24 @@ class MainWindow(qtw.QDialog):
 
         self.setImage()
         lettersWidget.setLayout(self.letterLayout)
-        
+
         hangmanLayout.addWidget(lettersWidget, 30)
 
         gameMainWidget.setLayout(hangmanLayout)
-        
+
         # gameMainWidget.setStyleSheet("background-color:red;")
         layout.addWidget(gameMainWidget, 0, 2, 5, 5)
         goBackToLobby = qtw.QPushButton("Go Back!")
         goBackToLobby.clicked.connect(self.goBack)
         layout.addWidget(goBackToLobby, 6, 6, 1, 1)
-        
+
         self.GameScene.setLayout(layout)
 
     def goBack(self):
         self.QtStack.setCurrentWidget(self.WaitingRoom)
 
-        self.guessedpassword = ['_' for _ in self.password]
-        self.passwordLabel.setText(' '.join(self.guessedpassword))
+        self.guessedpassword = ["_" for _ in self.password]
+        self.passwordLabel.setText(" ".join(self.guessedpassword))
         self.passwordLabel
 
         self.playersTable = {}
@@ -264,8 +259,7 @@ class MainWindow(qtw.QDialog):
             letterButton.clicked.connect(self.gameLogic)
             self.letterLayout.addWidget(letterButton)
         self.setImage()
-        
-        
+
     def gameLogic(self):
 
         letter = self.sender().text()
@@ -280,12 +274,13 @@ class MainWindow(qtw.QDialog):
                 for id in self.findAllOccurencies(letter):
                     self.guessedpassword[id] = letter
 
-                self.passwordLabel.setText(' '.join(self.guessedpassword))
+                self.passwordLabel.setText(" ".join(self.guessedpassword))
 
-                if self.password == ''.join(self.guessedpassword):
+                if self.password == "".join(self.guessedpassword):
                     self.deleteAllLetters()
                     self.passwordLabel.setText(
-                        ' '.join(self.guessedpassword)+"\n\nYou Won!")
+                        " ".join(self.guessedpassword) + "\n\nYou Won!"
+                    )
             else:
                 self.com.addTexttoQueue(Message("0", Message.guessed_letter))
                 self.lifeCounter += 1
@@ -293,7 +288,8 @@ class MainWindow(qtw.QDialog):
 
             if self.lifeCounter == 4:
                 self.passwordLabel.setText(
-                    ' '.join(self.guessedpassword)+"\n\nYou Lost! ;-)")
+                    " ".join(self.guessedpassword) + "\n\nYou Lost! ;-)"
+                )
         else:
             self.lifeCounter += 1  # to handle click send on create
 
@@ -310,8 +306,8 @@ class MainWindow(qtw.QDialog):
 
     def setPassword(self, password: str):
         self.password = password.upper()
-        self.guessedpassword = ['_' for _ in password]
-        self.passwordLabel.setText(' '.join(self.guessedpassword))
+        self.guessedpassword = ["_" for _ in password]
+        self.passwordLabel.setText(" ".join(self.guessedpassword))
 
     def terminate(self):
         Qt.QCoreApplication.quit()
@@ -325,7 +321,7 @@ class LetterButton(qtw.QPushButton):
         self.animateClick(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
     w = MainWindow()
     sys.exit(app.exec())
