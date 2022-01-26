5b6856fac3cd94e6b2752edabbc6854c798d61a9
mkwarta <Michu1164@gmail.com>
2022-01-26 17:45:49 +0100

    add makefile

--- Server/MessageBuilder.cpp
+++ Server/MessageBuilder.cpp
@@ -42,37 +42,3 @@ std::ostream& operator<<(std::ostream& stream, const MessageBuilder& builder)
     return stream;
 }
 
-int main()
-{
-    MessageBuilder new_player(NEW_PLAYER, "zeev", 7);
-    std::cout << new_player << std::endl;
-    std::cout << new_player.serialize() << std::endl;
-    std::cout << "--------" << std::endl;
-
-    MessageBuilder nick_taken(NICK_TAKEN, "zeev", 4+1+2);
-    std::cout << nick_taken << std::endl;
-    std::cout << nick_taken.serialize() << std::endl;
-    std::cout << "--------" << std::endl;
-
-    MessageBuilder reset(RESET, "", 1);
-    std::cout << reset << std::endl;
-    std::cout << reset.serialize() << std::endl;
-    std::cout << "--------" << std::endl;
-
-    MessageBuilder win(WINNER, "wygrany", 10);
-    std::cout << win << std::endl;
-    std::cout << win.serialize() << std::endl;
-    std::cout << "--------" << std::endl;
-
-    MessageBuilder guess(GUESS, "", 1+2);
-    std::cout << guess << std::endl;
-    std::cout << guess.serialize() << std::endl;
-    std::cout << "--------" << std::endl;
-
-    MessageBuilder host_ready(HOST_READY, "", 1+2);
-    std::cout << host_ready << std::endl;
-    std::cout << host_ready.serialize() << std::endl;
-    std::cout << "--------" << std::endl;
-
-    return 0;
-}
